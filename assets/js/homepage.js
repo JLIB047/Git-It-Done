bcafd9f0c8e34633ce8ef32ede589d52344ad42f
Jason Libertelli <jason.libertelli@gmail.com>
2021-11-02 23:03:04 -0400

    add language specs

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event){
     //prevent page from refreshing 
@@ -86,5 +87,33 @@ var displayRepos = function(repos, searchTerm){
     }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:feature&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if(response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items,language);
+            });
+        }
+        else {
+            alert("Error: Github User Not Found");
+        }
+    });
+}
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+    
+    if(language) {
+        getFeaturedRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+    }
+    
+    
+}
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 
